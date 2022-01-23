f63d577f7411ca821baf253d03af45cb98ce9b1a
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2022-01-23 16:18:31 -0500

    Updated

--- mail_handler.php
+++ mail_handler.php
@@ -7,13 +7,13 @@ if(isset($_POST['submit'])){
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message2 = "Voici une copie de votre message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Courriel envoyé. Merci " . $first_name . ", nous vous contacterons dans les plus bref délais.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
